9b27a2ad8d4cfb069fec4d6999a4b8bbf608d2f2
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-06-01 12:47:24 -0500

    molecule MCMC

--- homework/2017-01/hw_5/Solucion/plots_canal_ionico.py
+++ homework/2017-01/hw_5/Solucion/plots_canal_ionico.py
@@ -4,26 +4,25 @@ import matplotlib.pyplot as plt
 def plot_file(name):
     fig, (ax1, ax2) = plt.subplots(1, 2)
     x, y = np.genfromtxt("%s.txt"%name).T
-    chain = np.genfromtxt('%s_out.dat'%name, skip_header = 1)
+    xs, ys, r = np.genfromtxt('%s_out.dat'%name).T
     constants = open('%s_out.dat'%name).readline()
-    X0, Y0 = constants.split(' ')[:2]
-    X0, Y0 = float(X0), float(Y0)
 
-    x = x
-    y = y
     ax1.plot(x, y, 'o')
     theta = np.linspace(0, 2*np.pi, 100)
 
-    r = chain.mean()
-    x = r*np.cos(theta) + X0
-    y = r*np.sin(theta) + Y0
+    pos = r.argmax()
+    r_ = r[pos].mean()
+    X0 = xs[pos]
+    Y0 = ys[pos]
+    x = r_*np.cos(theta) + X0
+    y = r_*np.sin(theta) + Y0
 
     ax1.plot(x, y)
     ax1.plot([X0], [Y0], "o")
     ax1.set_xlabel('$x$')
     ax1.set_ylabel('$y$')
-    ax1.set_title('$r = %.3f$, $x_0 = %.3f$, $y_0 = %.3f$'%(r, X0, Y0))
-    ax2.hist(chain)
+    ax1.set_title('$r = %.3f$, $x_0 = %.3f$, $y_0 = %.3f$'%(r_, X0, Y0))
+    ax2.hist(r)
     ax1.axis("equal")
     ax2.set_ylabel('Frequency')
     ax2.set_xlabel('$r$')
