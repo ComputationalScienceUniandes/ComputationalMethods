e3958ea257913080f6284f3e3dc45dc6f8608eb3
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-07-21 18:52:49 -0500

    end changes

--- homework/2017-19/hw_3/Monitor_hw3/Tarea3.mk
+++ homework/2017-19/hw_3/Monitor_hw3/Tarea3.mk
@@ -1,22 +1,22 @@
 WAVE = 30.png 60.png
 PLANETS = planets.png
-erase = rm -r -f *.dat a.out *.png *.aux *.log *.mp4
+erase = rm -r -f *.dat a.out *.png *.aux *.log
 
 Resultados_hw3.pdf : Resultados_hw3.tex $(WAVE) $(PLANETS)
 	pdflatex $<
-	$(erase)
+	# $(erase)
 
 $(WAVE) : Onda.py
 	python $<
 
-$(PLANETS) : *.dat Plots_Planetas
+$(PLANETS) : *.dat Plots_Planetas.py
 	python Plots_Planetas.py
 
-*.dat : a.out
+*.dat : a.out coordinates.csv
 	./a.out
 
 a.out : planetas.c planetas.h
 	gcc planetas.c -lm -fopenmp -O2
 
 clean :
-	$(erase) *.pdf
+	$(erase) *.pdf *.mp4
