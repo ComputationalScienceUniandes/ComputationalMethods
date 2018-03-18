40205900bd3b9edea1a7d625c6078abb26229c7d
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2018-03-18 15:37:38 -0500

    cpp

---
+++ secciones/09.C++/apuesta.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+
+int main(){
+  int posicion;
+  int step;
+  int i;
+  int max_value;
+  srand(time(0));
+
+  posicion = 100;
+  i = 0;
+  while(posicion>0){
+    step = rand()%3 -1;
+    posicion += step;
+    cout << i << " "<< posicion << endl;
+    i++;
+  }
+  return 0;
+}
